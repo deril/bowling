f3f77ab66cdeb0967d1da10183184e7c5540fb82
Dmytro Bihniak <dmytro.bihniak@inbank.ee>
2021-11-19 21:54:56 +0200

    Create empty lib with one class

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+# Add the spec directory to the load path
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+# Auto-load all Ruby files in the lib directory.
+Dir["#{File.dirname(__FILE__)}/../lib/*.rb"].sort.each { |file| require file }
